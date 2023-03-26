5f0858d203d6c51608de9c0c70ff6a645018c3df
dinhluyen17 <118953522+dinhluyen17@users.noreply.github.com>
2023-03-26 22:48:39 +0700

    bvc

--- annotation/src/main/java/com/example/annotation/App.java
+++ annotation/src/main/java/com/example/annotation/App.java
@@ -3,14 +3,19 @@ package com.example.annotation;
 import com.example.annotation.annotation.VeryImportant;
 import com.example.annotation.entity.Cat;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class App {
-    public static void main(String[] args) throws NoSuchMethodException {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
          Cat myCat = new Cat("Henry");
          if(myCat.getClass().isAnnotationPresent(VeryImportant.class)) {
              System.out.println("Annotated!");
          }
         System.out.println(myCat.getClass());
         Class<? extends Cat> aClass = myCat.getClass();
-        aClass.getMethod("meow").;
+        Method meow = aClass.getMethod("meow");
+        meow.invoke(null);
+
     }
 }
