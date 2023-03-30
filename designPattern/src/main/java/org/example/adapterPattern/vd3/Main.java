d71c9c7b3d23c3d44637a6c7a655501cd845a210
dinhluyen17 <luyendd@mz.co.kr>
2023-03-30 11:28:57 +0700

    fd

--- designPattern/src/main/java/org/example/adapterPattern/vd3/Main.java
+++ designPattern/src/main/java/org/example/adapterPattern/vd3/Main.java
@@ -7,5 +7,11 @@ public class Main {
         Volt v1 = adapterCharger.get6V();
         System.out.println(v);
         System.out.println(v1);
+        System.out.println("----");
+        AdapterCharger adapterCharger1 = new AdapterChargerObject();
+        Volt v2 = adapterCharger1.get6V();
+        Volt v3 = adapterCharger1.get12V();
+        System.out.println(v2);
+        System.out.println(v3);
     }
 }
